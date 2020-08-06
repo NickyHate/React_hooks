050cfd036e03fdbf9e8c62a6e5451b378910b532
Никита <nikitostokarev22@gmail.com>
2020-08-06 22:44:08 +0300

    test useState hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [x, setX] = useState(0);
+
+  const getSquare = () => {
+    setX(2);
+    const sum = x * x;
+    console.log(sum);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getSquare()}>Нажми</button>
     </div>
   );
 }
