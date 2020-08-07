ad1c5302e54597ea57aeb76aad4191dda71b5514
Никита <nikitostokarev22@gmail.com>
2020-08-07 22:03:50 +0300

    test arrays with useState

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const initialValues = {
+    car: "bmw",
+    model: "x5",
+    color: "black",
+  };
   const [x, setX] = useState(0);
+  const [value, setValue] = useState(initialValues);
+
   const sum = x * x;
   const getSquare = () => {
-    setX(x+1);      
+    setX(x + 1);
   };
-  const checkValue = useEffect(() => {
-  
-  },[x])
+  useEffect(() => {
+    fetch("https://worldtimeapi.org/api/timezone/Africa/Ceuta").then(
+      (response) => {
+        console.log(response.json());
+      }
+    );
+  });
   return (
     <div className="App">
       <div className="title">{x}</div>
       <p>{sum}</p>
-      <button onClick={getSquare}>Нажми меня</button>
+      <div onClick={getSquare}>Нажми меня</div>
+      
     </div>
   );
 }
