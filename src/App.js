052a9c681ddacc2be051449fd4b6434466fd72ea
Никита <nikitostokarev22@gmail.com>
2020-08-07 22:20:32 +0300

    test arrays and map items

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const initialValues = {
-    car: "bmw",
-    model: "x5",
-    color: "black",
-  };
+  const initialValues = [
+    { car: "bmv", model: "x5", isNew: true },
+    { car: "bmv", model: "x6", isNew: false },
+    { car: "audi", model: "q7", isNew: true },
+    { car: "ford", model: "focus", isNew: false },
+  ];
   const [x, setX] = useState(0);
-  const [value, setValue] = useState(initialValues);
+  const [cars, setCars] = useState(initialValues);
+
+  const getCars = () => {
+    setCars((prevState) => [
+      ...prevState,
+      { car: "lada", model: "priora", isNew: false },
+    ]);
+    console.log(cars);
+  };
 
   const sum = x * x;
   const getSquare = () => {
@@ -26,7 +35,18 @@ function App() {
       <div className="title">{x}</div>
       <p>{sum}</p>
       <div onClick={getSquare}>Нажми меня</div>
-      
+      <div onClick={getCars}>Законсолить машинки</div>
+      <div>
+        {cars.map((car) => {
+          return (
+            <>
+              <div>{car.car}</div>
+              <div>{car.model}</div>
+              <div>{car.isNew}</div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
